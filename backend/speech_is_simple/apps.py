c3e8e5a7ee4087aa88763a4ae6992d1e866cd894
RICTUS <parshintseva-s@mail.ru>
2024-04-23 01:02:49 +0500

    frontend init

--- backend/speech_is_simple/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpeechIsSimpleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'speech_is_simple'
--- backend/speech_is_simple_auth/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpeechIsSimpleAuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'speech_is_simple_auth'
