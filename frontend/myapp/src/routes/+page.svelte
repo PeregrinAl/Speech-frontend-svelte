c6b4753c3d564d016ee94ac0ae34cfbcbb8f21cb
RICTUS <parshintseva-s@mail.ru>
2024-04-23 19:50:16 +0500

    static path enabled for vite + login page designed + font added

--- frontend/myapp/src/routes/+page.svelte
+++ frontend/myapp/src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Hello and welcome to my site!</h1>
-<a href="/about">About my site</a>
+<script>
+
+
+</script>
+
+<style>
+@import "/static/styles.css";
+</style>
+
+<body>
+  <div>
+  <h1 class="h1Pink">Учиться говорить - легко!</h1>
+  <p>
+      Добро пожаловать в сервис 
+      «Говорить - легко»! 
+      <br>
+
+      <br>Мы здесь, чтобы помочь улучшить вашу речь<br>
+      
+      <br>Начните свой путь к улучшению речи вместе с нами сегодня!
+  </p>
+  </div>
+<div class ="container">
+<container>
+<form id="email" action="/apply/" method="POST">
+  <label>
+    Email:
+    <input
+      type="text"
+      name="name"
+      id="name"
+      placeholder="Email"
+      required
+    >
+  </label>
+
+  <label>
+    Пароль:
+    <input type="text" name="password" required>
+  </label>
+  <button type="submit">Войти</button>
+  <!-- <label>
+    <input type="checkbox" name="remember_me" value="1">
+    запомнить меня
+  </label> -->
+  <label>
+    Еще нет аккаунта?:
+    <button type="submit">Зарегистрироваться</button>
+  </label>
+  </form>
+</container>
+</body>
